0ff89081d4d311da9a08640ac43a6def275efeb9
khiempham16 <125805195+KhiemPham16@users.noreply.github.com>
2026-05-21 22:21:33 +0700

    block f12

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import DisableDevtool from 'disable-devtool';
+
 import GlobalStyles from './components/GlobalStyles';
 import App from './App.jsx';
+
+DisableDevtool({
+    disableMenu: true,
+    disableSelect: true,
+    clearIntervalWhenDevOpenTrigger: true,
+    ondevtoolopen(type, next) {
+        alert('DevTools detected');
+        next();
+    }
+});
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
