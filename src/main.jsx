ff2bc63640b3ffb846ee54f77fd142b806c6598b
khiempham16 <125805195+KhiemPham16@users.noreply.github.com>
2026-05-21 23:13:11 +0700

    fix: responsive projects and devtool check

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,17 @@ import DisableDevtool from 'disable-devtool';
 import GlobalStyles from './components/GlobalStyles';
 import App from './App.jsx';
 
-DisableDevtool({
-    disableMenu: true,
-    disableSelect: true,
-    clearIntervalWhenDevOpenTrigger: true,
-    ondevtoolopen(type, next) {
-        alert('DevTools detected');
-        next();
-    }
-});
+if (import.meta.env.PROD) {
+    DisableDevtool({
+        disableMenu: true,
+        disableSelect: true,
+        clearIntervalWhenDevOpenTrigger: true,
+        ondevtoolopen(type, next) {
+            alert('DevTools detected');
+            next();
+        }
+    });
+}
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
