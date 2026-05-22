aac25ae3805e3232b25be6a05ca39ac6744d051f
khiempham16 <125805195+KhiemPham16@users.noreply.github.com>
2026-05-22 23:02:11 +0700

    ref: unlock f12 fix: responesive projects

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import DisableDevtool from 'disable-devtool';
 
 import GlobalStyles from './components/GlobalStyles';
 import App from './App.jsx';
-
-if (import.meta.env.PROD) {
-    DisableDevtool({
-        disableMenu: true,
-        disableSelect: true,
-        clearIntervalWhenDevOpenTrigger: true,
-        ondevtoolopen(type, next) {
-            alert('DevTools detected');
-            next();
-        }
-    });
-}
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
